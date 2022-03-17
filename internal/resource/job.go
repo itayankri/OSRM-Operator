85c807e89861c6a7b20c314aede4da14b7ec88b3
Itay Ankri <itay.ankri@gmail.com>
2022-03-17 22:22:04 +0200

    moved map-builder from init-containers to independent job

--- internal/resource/job.go
+++ internal/resource/job.go
@@ -5,28 +5,33 @@ import (
 	"strings"
 
 	"github.com/itayankri/OSRM-Operator/internal/metadata"
-	appsv1 "k8s.io/api/apps/v1"
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
-type DeploymentBuilder struct {
+type JobBuilder struct {
 	ProfileScopedBuilder
 	*OSRMResourceBuilder
 }
 
-func (builder *OSRMResourceBuilder) Deployment(profile OSRMProfile) *DeploymentBuilder {
-	return &DeploymentBuilder{
+var profilesMap = map[OSRMProfile]string{
+	DrivingProfile: "car.lua",
+	CyclingProfile: "bycicling.lua",
+	FootProfile:    "foot.lua",
+}
+
+func (builder *OSRMResourceBuilder) Job(profile OSRMProfile) *JobBuilder {
+	return &JobBuilder{
 		ProfileScopedBuilder{profile},
 		builder,
 	}
 }
 
-func (builder *DeploymentBuilder) Build() (client.Object, error) {
-	return &appsv1.Deployment{
+func (builder *JobBuilder) Build() (client.Object, error) {
+	return &batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      fmt.Sprintf("%s-%s", builder.Instance.Name, builder.profile),
 			Namespace: builder.Instance.Namespace,
@@ -35,20 +40,13 @@ func (builder *DeploymentBuilder) Build() (client.Object, error) {
 	}, nil
 }
 
-func (builder *DeploymentBuilder) Update(object client.Object) error {
+func (builder *JobBuilder) Update(object client.Object) error {
 	name := fmt.Sprintf("%s-%s", builder.Instance.Name, builder.profile)
-	deployment := object.(*appsv1.Deployment)
 	pbfFileName := builder.Instance.Spec.GetPbfFileName()
 	osrmFileName := strings.ReplaceAll(pbfFileName, "osm.pbf", "osrm")
-	profileSpec := getProfileSpec(builder.profile, builder.Instance)
+	job := object.(*batchv1.Job)
 
-	deployment.Spec = appsv1.DeploymentSpec{
-		Replicas: profileSpec.MinReplicas,
-		Selector: &metav1.LabelSelector{
-			MatchLabels: map[string]string{
-				"app": name,
-			},
-		},
+	job.Spec = batchv1.JobSpec{
 		Template: corev1.PodTemplateSpec{
 			ObjectMeta: metav1.ObjectMeta{
 				Labels: metadata.GetLabels(builder.Instance.Name, builder.Instance.Labels),
@@ -56,13 +54,8 @@ func (builder *DeploymentBuilder) Update(object client.Object) error {
 			Spec: corev1.PodSpec{
 				Containers: []corev1.Container{
 					{
-						Name:  osrmContainerName,
+						Name:  fmt.Sprintf("%s-%s", name, "map-builder"),
 						Image: builder.Instance.Spec.GetImage(),
-						Ports: []corev1.ContainerPort{
-							{
-								ContainerPort: 5000,
-							},
-						},
 						Resources: corev1.ResourceRequirements{
 							Requests: map[corev1.ResourceName]resource.Quantity{
 								"memory": resource.MustParse("1Gi"),
@@ -70,10 +63,24 @@ func (builder *DeploymentBuilder) Update(object client.Object) error {
 							},
 						},
 						Command: []string{
-							"osrm-routed",
-							"--algorithm",
-							"mld",
-							fmt.Sprintf("%s/%s", osrmDataPath, osrmFileName),
+							"/bin/sh",
+							"-c",
+						},
+						Args: []string{
+							fmt.Sprintf(`
+								apt update && \
+								apt --assume-yes install wget && \
+								wget %s && \
+								osrm-extract -p %s %s && \
+								osrm-partition %s && \
+								osrm-customize %s
+							`,
+								builder.Instance.Spec.PBFURL,
+								profilesMap[builder.profile],
+								pbfFileName,
+								osrmFileName,
+								osrmFileName,
+							),
 						},
 						VolumeMounts: []corev1.VolumeMount{
 							{
@@ -96,10 +103,6 @@ func (builder *DeploymentBuilder) Update(object client.Object) error {
 				},
 			},
 		},
-	}
-
-	if err := controllerutil.SetControllerReference(builder.Instance, deployment, builder.Scheme); err != nil {
-		return fmt.Errorf("failed setting controller reference: %v", err)
 	}
 
 	return nil
