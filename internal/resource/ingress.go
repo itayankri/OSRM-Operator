b5706b055e6c4ec720a1853097e6eec6ed6da712
Itay Ankri <itay.ankri@gmail.com>
2022-03-19 11:28:27 +0200

    service selector fix

--- internal/resource/ingress.go
+++ internal/resource/ingress.go
@@ -3,6 +3,7 @@ package resource
 import (
 	"fmt"
 
+	osrmv1alpha1 "github.com/itayankri/OSRM-Operator/api/v1alpha1"
 	"github.com/itayankri/OSRM-Operator/internal/metadata"
 	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -33,25 +34,34 @@ func (builder *IngressBuilder) Build() (client.Object, error) {
 
 func (builder *IngressBuilder) Update(object client.Object) error {
 	ingress := object.(*networkingv1.Ingress)
-	rules := []networkingv1.IngressRule{}
+	paths := []networkingv1.HTTPIngressPath{}
 	services := []string{"route", "table"}
 
-	if builder.Instance.Spec.Ingress != nil && builder.Instance.Spec.Ingress.ExposingServices != nil {
+	if builder.Instance.Spec.Ingress.ExposingServices != nil {
 		services = builder.Instance.Spec.Ingress.ExposingServices
 	}
 
 	for _, profile := range builder.profiles {
 		for _, service := range services {
-			rule := builder.getIngressRule(profile, service)
-			rules = append(rules, *rule)
+			path := builder.getIngressRulePath(profile, service)
+			paths = append(paths, path)
 		}
 	}
 
 	ingress.Labels = metadata.GetLabels(builder.Instance.Name, builder.Instance.Labels)
+	ingress.Annotations = annotationsFactory(*builder.Instance.Spec.Ingress)
 
 	ingress.Spec = networkingv1.IngressSpec{
 		IngressClassName: builder.Instance.Spec.Ingress.IngressClassName,
-		Rules:            rules,
+		Rules: []networkingv1.IngressRule{
+			{
+				IngressRuleValue: networkingv1.IngressRuleValue{
+					HTTP: &networkingv1.HTTPIngressRuleValue{
+						Paths: paths,
+					},
+				},
+			},
+		},
 	}
 
 	if err := controllerutil.SetControllerReference(builder.Instance, ingress, builder.Scheme); err != nil {
@@ -61,39 +71,45 @@ func (builder *IngressBuilder) Update(object client.Object) error {
 	return nil
 }
 
-func (builder *IngressBuilder) getIngressRule(profile OSRMProfile, service string) *networkingv1.IngressRule {
+func (builder *IngressBuilder) getIngressRulePath(profile OSRMProfile, service string) networkingv1.HTTPIngressPath {
 	serviceName := fmt.Sprintf("%s-%s", builder.Instance.Name, profile)
-	var path string
-	var pathType networkingv1.PathType
 
-	if builder.Instance.Spec.Ingress != nil &&
-		builder.Instance.Spec.Ingress.IngressClassName != nil &&
-		*builder.Instance.Spec.Ingress.IngressClassName == "nginx" {
-		pathType = networkingv1.PathTypePrefix
-		path = fmt.Sprintf("/%s/v1/%s/.*", service, profile)
-	} else {
-		pathType = networkingv1.PathTypeImplementationSpecific
-		path = fmt.Sprintf("/%s/v1/%s/*", service, profile)
-	}
+	path, pathType := pathFactory(profile, service, builder.Instance.Spec.Ingress.IngressClassName)
 
-	return &networkingv1.IngressRule{
-		IngressRuleValue: networkingv1.IngressRuleValue{
-			HTTP: &networkingv1.HTTPIngressRuleValue{
-				Paths: []networkingv1.HTTPIngressPath{
-					{
-						Path:     path,
-						PathType: &pathType,
-						Backend: networkingv1.IngressBackend{
-							Service: &networkingv1.IngressServiceBackend{
-								Name: serviceName,
-								Port: networkingv1.ServiceBackendPort{
-									Number: 80,
-								},
-							},
-						},
-					},
+	return networkingv1.HTTPIngressPath{
+		Path:     path,
+		PathType: &pathType,
+		Backend: networkingv1.IngressBackend{
+			Service: &networkingv1.IngressServiceBackend{
+				Name: serviceName,
+				Port: networkingv1.ServiceBackendPort{
+					Number: 80,
 				},
 			},
 		},
+	}
+}
+
+func pathFactory(profile OSRMProfile, service string, ingressClassName *string) (string, networkingv1.PathType) {
+	if ingressClassName != nil {
+		if *ingressClassName == "nginx" {
+			return fmt.Sprintf("/%s/v1/%s/.*", service, profile), networkingv1.PathTypePrefix
+		}
+	}
+
+	return fmt.Sprintf("/%s/v1/%s/*", service, profile), networkingv1.PathTypeImplementationSpecific
+}
+
+func annotationsFactory(ingressSpec osrmv1alpha1.IngressSpec) map[string]string {
+	if ingressSpec.IngressClassName != nil {
+		if *ingressSpec.IngressClassName == "nginx" {
+			return map[string]string{
+				"nginx.ingress.kubernetes.io/use-regex": "true",
+			}
+		}
+	}
+
+	return map[string]string{
+		"kubernetes.io/ingress.global-static-ip-name": ingressSpec.IPName,
 	}
 }
