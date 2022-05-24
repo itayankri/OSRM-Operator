2c30ca0fa27602ab9134812cbb48b22159ca0542
Itay Ankri <itay.ankri@gmail.com>
2022-05-24 21:52:17 +0300

    resource module tests entrypoint

---
+++ internal/resource/suite_test.go
@@ -0,0 +1,13 @@
+package resource_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestTests(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Cache Tests Suite")
+}
