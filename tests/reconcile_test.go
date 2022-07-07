d638d744c0f3d45c0e5d539d60fff80a2e6bcc9d
Itay Ankri <itay.ankri@gmail.com>
2022-07-07 20:34:35 +0300

    wip

--- tests/reconcile_test.go
+++ tests/reconcile_test.go
@@ -3,17 +3,22 @@ package tests_test
 import (
 	"context"
 
-	osrmv1alpha1 "github.com/itayankri/OSRM-Operator/api/v1alpha1"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	osrmv1alpha1 "github.com/itayankri/OSRM-Operator/api/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
 )
 
 var _ = Context("Reconcile", func() {
 	var cluster *osrmv1alpha1.OSRMCluster
+	var k8sClient client.Client
 
 	BeforeEach(func() {
+		k8sClient, _ = client.New(config.GetConfigOrDie(), client.Options{})
 		cluster = getOSRMClusterCR()
-		Expect(client.Create(context.Background(), cluster)).To(Succeed())
+		Expect(k8sClient.Create(context.Background(), cluster)).To(Succeed())
 	})
 
 	Describe("Profile updates", func() {
