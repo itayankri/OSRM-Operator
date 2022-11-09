14a1c94be346b689b02f45ed2cef57554c70c13c
Itay Ankri <itay.ankri@gmail.com>
2022-11-09 16:33:13 +0200

    wip

--- internal/resource/osrm_resource_builder.go
+++ internal/resource/osrm_resource_builder.go
@@ -42,7 +42,7 @@ func (builder *OSRMResourceBuilder) ResourceBuilders() []ResourceBuilder {
 	if len(builders) > 0 {
 		builders = append(builders, []ResourceBuilder{
 			builder.ConfigMap(builder.Instance.Spec.Profiles),
-			builder.GatewayServiceBuilder(builder.Instance.Spec.Profiles),
+			builder.GatewayService(builder.Instance.Spec.Profiles),
 			builder.GatewayDeployment(builder.Instance.Spec.Profiles),
 		}...)
 	}
