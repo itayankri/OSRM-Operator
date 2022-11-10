377b41b91bc1d1af0b148cce431db02379840805
Itay Ankri <itay.ankri@gmail.com>
2022-11-10 12:34:26 +0200

    wip

--- main.go
+++ main.go
@@ -82,6 +82,7 @@ func main() {
 	if err = (&controllers.OSRMClusterReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
+		log:    ctrl.Log.WithName("controller").WithName("OSRM"),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "OSRMCluster")
 		os.Exit(1)
