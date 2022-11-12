db97c109a866e69f503513e1ee4b1f574713c159
Itay Ankri <itay.ankri@gmail.com>
2022-11-12 18:39:18 +0200

    Resource requirements

--- api/v1alpha1/osrmcluster_types.go
+++ api/v1alpha1/osrmcluster_types.go
@@ -20,6 +20,7 @@ import (
 	"strings"
 
 	"github.com/itayankri/OSRM-Operator/internal/status"
+	corev1 "k8s.io/api/core/v1"
 	resource "k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
@@ -84,10 +85,18 @@ func (spec *OSRMClusterSpec) GetPbfFileName() string {
 type ProfilesSpec []*ProfileSpec
 
 type ProfileSpec struct {
-	Name         string `json:"name,omitempty"`
-	EndpointName string `json:"endpointName,omitempty"`
-	MinReplicas  *int32 `json:"minReplicas,omitempty"`
-	MaxReplicas  *int32 `json:"maxReplicas,omitempty"`
+	Name         string                       `json:"name,omitempty"`
+	EndpointName string                       `json:"endpointName,omitempty"`
+	MinReplicas  *int32                       `json:"minReplicas,omitempty"`
+	MaxReplicas  *int32                       `json:"maxReplicas,omitempty"`
+	Resources    *corev1.ResourceRequirements `json:"resources,omitempty"`
+}
+
+func (spec *ProfileSpec) GetResources() *corev1.ResourceRequirements {
+	if spec.Resources == nil {
+		return &corev1.ResourceRequirements{}
+	}
+	return spec.Resources
 }
 
 type ServiceSpec struct {
