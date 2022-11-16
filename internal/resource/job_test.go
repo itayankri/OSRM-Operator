b6825b09a8e13ef212ac38df9e4dbf893f48ef22
Itay Ankri <itay.ankri@gmail.com>
2022-11-16 14:23:02 +0200

    job tests

---
+++ internal/resource/job_test.go
@@ -0,0 +1,22 @@
+package resource_test
+
+import (
+	"github.com/itayankri/OSRM-Operator/internal/resource"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+var _ = Describe("Job builder", func() {
+	Context("ShouldDeploy", func() {
+		var builder resource.ResourceBuilder
+		BeforeEach(func() {
+			builder = osrmResourceBuilder.Job(instance.Spec.Profiles[0])
+		})
+
+		It("Should always return 'true'", func() {
+			resources := []runtime.Object{}
+			Expect(builder.ShouldDeploy(resources)).To(Equal(true))
+		})
+	})
+})
