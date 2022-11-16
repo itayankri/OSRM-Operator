2fadcb09934f1445be368a20c13d48a931b2d66c
Itay Ankri <itay.ankri@gmail.com>
2022-11-16 14:04:50 +0200

    service builder tests

--- internal/resource/service_test.go
+++ internal/resource/service_test.go
@@ -1,75 +1,66 @@
 package resource_test
 
 import (
+	"fmt"
+
 	osrmv1alpha1 "github.com/itayankri/OSRM-Operator/api/v1alpha1"
 	"github.com/itayankri/OSRM-Operator/internal/resource"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
-	k8sresource "k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/runtime"
 	defaultscheme "k8s.io/client-go/kubernetes/scheme"
 )
 
-func generateOSRMCluster() osrmv1alpha1.OSRMCluster {
-	minReplicas := int32(2)
-	maxReplicas := int32(4)
-	storage := k8sresource.MustParse("100Mi")
-	return osrmv1alpha1.OSRMCluster{
-		Spec: osrmv1alpha1.OSRMClusterSpec{
-			PBFURL: "https://download.geofabrik.de/asia/israel-and-palestine-latest.osm.pbf",
-			Profiles: osrmv1alpha1.ProfilesSpec{
-				{
-					Name:         "car",
-					EndpointName: "driving",
-					MinReplicas:  &minReplicas,
-					MaxReplicas:  &maxReplicas,
-				},
-			},
-			Service: osrmv1alpha1.ServiceSpec{
-				ExposingServices: []string{"route", "table"},
-			},
-			Persistence: osrmv1alpha1.PersistenceSpec{
-				StorageClassName: "standard",
-				Storage:          &storage,
-			},
-		},
-	}
-}
-
-var _ = Context("Services", func() {
+var _ = Describe("Service builder", func() {
 	var (
-		instance osrmv1alpha1.OSRMCluster
-		builder  resource.OSRMResourceBuilder
-		scheme   *runtime.Scheme
+		scheme         *runtime.Scheme
+		serviceBuilder resource.ResourceBuilder
 	)
 
-	Describe("Build", func() {
-		BeforeEach(func() {
-			scheme = runtime.NewScheme()
-			Expect(osrmv1alpha1.AddToScheme(scheme)).To(Succeed())
-			Expect(defaultscheme.AddToScheme(scheme)).To(Succeed())
-			instance = generateOSRMCluster()
-			builder = resource.OSRMResourceBuilder{
-				Instance: &instance,
-				Scheme:   scheme,
-			}
-		})
+	BeforeEach(func() {
+		scheme = runtime.NewScheme()
+		Expect(osrmv1alpha1.AddToScheme(scheme)).To(Succeed())
+		Expect(defaultscheme.AddToScheme(scheme)).To(Succeed())
+		serviceBuilder = osrmResourceBuilder.Service(instance.Spec.Profiles[0])
+	})
 
+	Context("Build", func() {
 		It("Should use values from custom resource", func() {
-			serviceBuilder := builder.Service(instance.Spec.Profiles[0])
 			obj, err := serviceBuilder.Build()
 			Expect(err).NotTo(HaveOccurred())
 			service := obj.(*corev1.Service)
 
 			By("generates a service object with the correct name and labels", func() {
-				expectedName := instance.Name
+				expectedName := fmt.Sprintf("%s-%s", instance.ObjectMeta.Name, instance.Spec.Profiles[0].Name)
 				Expect(service.Name).To(Equal(expectedName))
 			})
 
 			By("generates a service object with the correct namespace", func() {
 				Expect(service.Namespace).To(Equal(instance.Namespace))
 			})
+		})
+	})
+
+	Context("ShouldDeploy", func() {
+		It("Should return 'false' when both PVC is bound and map builder Job is not completed yet", func() {
+			resources := generateChildResources(false, false, instance.ObjectMeta.Name, instance.Spec.Profiles[0].Name)
+			Expect(serviceBuilder.ShouldDeploy(resources)).To(Equal(false))
+		})
+
+		It("Should return 'false' when PVC is bound but map builder Job is not completed yet", func() {
+			resources := generateChildResources(true, false, instance.ObjectMeta.Name, instance.Spec.Profiles[0].Name)
+			Expect(serviceBuilder.ShouldDeploy(resources)).To(Equal(false))
+		})
+
+		It("Should return 'false' when PVC is not bound but map builder Job is completed", func() {
+			resources := generateChildResources(false, true, instance.ObjectMeta.Name, instance.Spec.Profiles[0].Name)
+			Expect(serviceBuilder.ShouldDeploy(resources)).To(Equal(false))
+		})
+
+		It("Should return 'true' when both PVC is bound and map builder Job is compoleted", func() {
+			resources := generateChildResources(true, true, instance.ObjectMeta.Name, instance.Spec.Profiles[0].Name)
+			Expect(serviceBuilder.ShouldDeploy(resources)).To(Equal(true))
 		})
 	})
 })
