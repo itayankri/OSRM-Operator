c11c370d73602de3b09ef1f0a0a34df9b3a6ded1
Itay Ankri <itay.ankri@gmail.com>
2023-01-13 15:00:11 +0200

    bash script adjustments

--- docker/map-builder/run.sh
+++ docker/map-builder/run.sh
@@ -30,22 +30,31 @@ fi
 
 PBF_FILE_NAME=$(basename $PBF_URL)
 OSRM_FILE_NAME="${PBF_FILE_NAME/osm.pbf/osrm}"
+TEMP_DIR=$(date "+%F-%T")
 
 cd $ROOT_DIR
-mkdir $PARTITIONED_DATA_DIR $CUSTOMIZED_DATA_DIR
-cd $PARTITIONED_DATA_DIR
+mkdir -p $PARTITIONED_DATA_DIR $CUSTOMIZED_DATA_DIR $TEMP_DIR
 
-echo "Downloading PBF file from $PBF_URL"
+cd $TEMP_DIR
+
+echo "--> Downloading PBF file from $PBF_URL"
 curl -O $PBF_URL
 
-echo "Extracting PBF"
+echo "--> Extracting PBF"
 osrm-extract -p /opt/$PROFILE.lua $PBF_FILE_NAME && \
 
-echo "Partitioning map data"
+echo "--> Partitioning map data"
 osrm-partition $OSRM_FILE_NAME
 
-cp * ../$CUSTOMIZED_DATA_DIR
-cd ../$CUSTOMIZED_DATA_DIR
+echo "--> Copying partitioned data to $PARTITIONED_DATA_DIR"
+cp * ../$PARTITIONED_DATA_DIR
 
-echo "Customizing map data"
+echo "--> Customizing map data"
 osrm-customize $OSRM_FILE_NAME
+
+echo "--> Copying customized ata to $CUSTOMIZED_DATA_DIR"
+cp * ../$CUSTOMIZED_DATA_DIR
+
+echo "--> Cleaning up temp dir $TEMP_DIR"
+cd $ROOT_DIR
+rm -rf $TEMP_DIR
